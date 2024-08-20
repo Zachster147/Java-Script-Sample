b882c24cfcf4059e49a424c90d509d92df32276c
Zachster147 <ze.villamor@my.smciligan.edu.ph>
2024-08-20 16:27:00 +0800

    Create JS code.js

---
+++ JS code.js
@@ -0,0 +1,9 @@
+const a = Math.random();
+console.log(a);
+
+
+Math.random() * (highestNumber - lowestNumber) + lowestNumber
+
+const b = Math.random() * (10-1) + 1
+console.log(a);
+console.log(`Random value between 1 and 10 is ${b}`);
